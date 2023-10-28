0bd0ea962f783f62dfd3dc0c491d940142a17d75
Nguyen Duc Thang <aloducthang@gmail.com>
2023-10-28 19:06:46 +0700

    report

--- src/components/StaffViewReportContainer/index.js
+++ src/components/StaffViewReportContainer/index.js
@@ -1,7 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import classNames from 'classnames/bind';
 import style from './StaffViewReportContainer.module.scss';
-
-import React, { useEffect, useState } from 'react';
+import './datepicker.css';
 
 const cx = classNames.bind(style);
 
@@ -20,10 +22,61 @@ function Report() {
         //     });
     }, []);
 
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+
+    const handleStartDateChange = (date) => {
+        setStartDate(date);
+        if (endDate && date > endDate) {
+            setEndDate(date);
+        }
+    };
+
+    const handleEndDateChange = (date) => {
+        if (startDate && date < startDate) {
+            setStartDate(date);
+        }
+        setEndDate(date);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Start Date:', startDate.toLocaleDateString('en-GB'));
+        console.log('End Date:', endDate.toLocaleDateString('en-GB'));
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('container')}>
                 <h2 className={cx('title')}>Report</h2>
+
+                <form className={cx('form-container')} onSubmit={handleSubmit}>
+                    <div className={cx('form-group')}>
+                        <label className={cx('label')} htmlFor="start-date">Start Date:</label>
+                        <DatePicker
+                            id="start-date"
+                            className={cx('form-control')}
+                            selected={startDate}
+                            onChange={handleStartDateChange}
+                            dateFormat="dd/MM/yyyy"
+                            isClearable
+                        />
+                    </div>
+                    <div className={cx('form-group')}>
+                        <label className={cx('label')} htmlFor="end-date">End Date:</label>
+                        <DatePicker
+                            id="end-date"
+                            className={cx('form-control')}
+                            selected={endDate}
+                            onChange={handleEndDateChange}
+                            dateFormat="dd/MM/yyyy"
+                            isClearable
+                            minDate={startDate}
+                        />
+                    </div>
+                    <button className={cx('button')} type="submit">Submit</button>
+                </form>
+
                 {reportData.length > 0 ? (
                     <table className={cx('table')}>
                         <thead>
