3581543468c3694f2fde6c2e6a681b96a6100138
Nguyen Duc Thang <aloducthang@gmail.com>
2023-10-28 19:19:57 +0700

    chỉnh staff page

--- src/components/StaffSidebar/index.js
+++ src/components/StaffSidebar/index.js
@@ -11,47 +11,35 @@ function StaffSidebar() {
         <div className={cx('wrapper')}>
             <div className={cx('sidebar')}>
                 <div className={cx('category')}>
-                    <h3 className={cx('title')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faClipboardUser}></FontAwesomeIcon>
-                        <span className={cx('span')}>Staff</span>
-                    </h3>
                     <Link to={'/staff/view-staff'} className={cx('function')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faCaretRight}></FontAwesomeIcon>
-                        View <span className={cx('span')}>Staff</span>
+                        <p className={cx('title')}>
+                            <FontAwesomeIcon className={cx('icon')} icon={faClipboardUser}></FontAwesomeIcon>
+                            <span className={cx('span')}>Staff</span>
+                        </p>
                     </Link>
                 </div>
                 <div className={cx('category')}>
-                    <h3 className={cx('title')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faMessage}></FontAwesomeIcon>
-                        <span className={cx('span')}>Feedback</span>
-                    </h3>
                     <Link to={'/staff/view-feedback'} className={cx('function')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faCaretRight}></FontAwesomeIcon>
-                        View <span className={cx('span')}>Feedback</span>
+                        <p className={cx('title')}>
+                            <FontAwesomeIcon className={cx('icon')} icon={faMessage}></FontAwesomeIcon>
+                            <span className={cx('span')}>Feedback</span>
+                        </p>
                     </Link>
                 </div>
                 <div className={cx('category')}>
-                    <h3 className={cx('title')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faScrewdriverWrench}></FontAwesomeIcon>
-                        <span className={cx('span')}>Repair</span>
-                    </h3>
-                    <Link to={'/staff/add-history'} className={cx('function')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faCaretRight}></FontAwesomeIcon>
-                        Add <span className={cx('span')}>History</span>
-                    </Link>
                     <Link to={'/staff/view-history'} className={cx('function')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faCaretRight}></FontAwesomeIcon>
-                        View <span className={cx('span')}>History</span>
+                        <p className={cx('title')}>
+                            <FontAwesomeIcon className={cx('icon')} icon={faScrewdriverWrench}></FontAwesomeIcon>
+                            <span className={cx('span')}>Repair</span>
+                        </p>
                     </Link>
                 </div>
                 <div className={cx('category')}>
-                    <h3 className={cx('title')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faChartColumn}></FontAwesomeIcon>
-                        <span className={cx('span')}>Report</span>
-                    </h3>
                     <Link to={'/staff/view-report'} className={cx('function')}>
-                        <FontAwesomeIcon className={cx('icon')} icon={faCaretRight}></FontAwesomeIcon>
-                        View <span className={cx('span')}>Report</span>
+                        <p className={cx('title')}>
+                            <FontAwesomeIcon className={cx('icon')} icon={faChartColumn}></FontAwesomeIcon>
+                            <span className={cx('span')}>Report</span>
+                        </p>
                     </Link>
                 </div>
             </div>
